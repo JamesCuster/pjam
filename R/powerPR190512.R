189ea12b66def9697e24022e4fa829e2a2b700f7
James Custer <jamescuster0121@gmail.com>
2019-06-19 12:14:26 -0500

    initial commit

---
+++ R/powerPR190512.R
@@ -0,0 +1,24 @@
+# Things to do.
+#   - Rename this script
+
+#' Design Constructor (breif description goes here)
+#'
+#' @param x a list of "factors" used in simulation design
+#'
+#' @return A longer description of what the function does can go here.
+#'
+sim.design.fn <- function(x) {
+  for (i in (length(x) - 1):1) {
+    x[[i]] <- rep(x[[i]], each = length(x[[i + 1]]))
+  }
+  return(data.frame(x))
+}
+
+
+# Possible alternative...not sure because I'm not sure what the ultimate goal is
+alternative <- function(x) {
+  i <- which.max(lapply(foo, length))
+  foo[[i]] <- rep(foo[[i]], each = length(foo[[i]]))
+  return(data.frame(foo))
+}
+

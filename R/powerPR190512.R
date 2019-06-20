1590f3b87d4fc61eb74b8aabe76c6a419800710b
James Custer <jamescuster0121@gmail.com>
2019-06-20 11:48:52 -0500

    add the power.lm.fn function and documnetation

--- R/powerPR190512.R
+++ R/powerPR190512.R
@@ -7,6 +7,8 @@
 #'
 #' @return A longer description of what the function does can go here.
 #'
+#' @export
+#'
 sim.design.fn <- function(x) {
   for (i in (length(x) - 1):1) {
     x[[i]] <- rep(x[[i]], each = length(x[[i + 1]]))
@@ -14,14 +16,45 @@ sim.design.fn <- function(x) {
   return(data.frame(x))
 }
 
+# Possible alternatives...not sure because I'm not sure what the ultimate goal is
 foo <- list(apple = c("hello", "world"), bannana = c("green", "yellow", "brown"), orange = c("blood", "small"))
 # This function may be doing what expand grid does
 expand.grid(foo)
 
-# Possible alternative...not sure because I'm not sure what the ultimate goal is
 alternative <- function(x) {
-  i <- which.max(lapply(foo, length))
-  foo[[i]] <- rep(foo[[i]], each = length(foo[[i]]))
-  return(data.frame(foo))
+  i <- which.max(lapply(x, length))
+  x[[i]] <- rep(x[[i]], each = length(x[[i]]))
+  return(data.frame(x))
+}
+
+
+#' Compute power in linear model
+#'
+#' @param n sample size
+#' @param alpha significance level (type 1 error rate)
+#' @param dfTest number of degrees of freedom for test
+#' @param dfAdj adjusted degrees of freedom
+#' @param R2full r squared full
+#' @param R2red r squared reduced
+#'
+#' @return Compute power in linear model (more description)
+#'
+#' @examples
+#' power.lm.fn(n = 180, alpha = 0.05, dfTest = 2, dfAdj = 4 + 1, R2full = 0.1, R2red = 0)
+#'
+#' @export
+#'
+power.lm.fn <- function(n, alpha, dfTest, dfAdj, R2full, R2red) {
+  R2part <- (R2full - R2red) / (1 - R2red)
+  lambda <- n * (R2full - R2red) / (1 - R2full)
+  power <-
+    stats::pf(
+      q = stats::qf(1 - alpha, df1 = dfTest, df2 = n - dfTest - dfAdj),
+      df1 = dfTest,
+      df2 = n - dfTest - dfAdj,
+      ncp = lambda,
+      lower.tail = FALSE
+    )
+  return(power)
 }
 

c8010f0a56940e10bd3fddb2e0799dd4a6c4aa10
James Custer <jamescuster0121@gmail.com>
2019-06-20 12:07:32 -0500

    code cleaned with lintr

--- R/powerPR190512.R
+++ R/powerPR190512.R
@@ -3,6 +3,9 @@
 
 #' Design Constructor (breif description goes here)
 #'
+#' You can add a slightly longer description here which is displayed underneath
+#' the brief description
+#'
 #' @param x a list of "factors" used in simulation design
 #'
 #' @return A longer description of what the function does can go here.
@@ -16,8 +19,12 @@ sim.design.fn <- function(x) {
   return(data.frame(x))
 }
 
-# Possible alternatives...not sure because I'm not sure what the ultimate goal is
-foo <- list(apple = c("hello", "world"), bannana = c("green", "yellow", "brown"), orange = c("blood", "small"))
+# Possible alternatives...not sure because I'm not sure what the ultimate goal
+# is
+foo <- list(
+  apple = c("hello", "world"),
+  bannana = c("green", "yellow", "brown"),
+  orange = c("blood", "small"))
 # This function may be doing what expand grid does
 expand.grid(foo)
 
@@ -30,31 +37,48 @@ alternative <- function(x) {
 
 #' Compute power in linear model
 #'
+#' Slightly longer description here
+#'
 #' @param n sample size
 #' @param alpha significance level (type 1 error rate)
-#' @param dfTest number of degrees of freedom for test
-#' @param dfAdj adjusted degrees of freedom
-#' @param R2full r squared full
-#' @param R2red r squared reduced
+#' @param df.test number of degrees of freedom for test
+#' @param df.adj adjusted degrees of freedom
+#' @param r2.full r squared full
+#' @param r2.red r squared reduced
 #'
 #' @return Compute power in linear model (more description)
 #'
 #' @examples
-#' power.lm.fn(n = 180, alpha = 0.05, dfTest = 2, dfAdj = 4 + 1, R2full = 0.1, R2red = 0)
+#' power.lm.fn(n = 180, alpha = 0.05, df.test = 2, df.adj = 4 + 1, r2.full = 0.1, r2.red = 0)
 #'
 #' @export
 #'
-power.lm.fn <- function(n, alpha, dfTest, dfAdj, R2full, R2red) {
-  R2part <- (R2full - R2red) / (1 - R2red)
-  lambda <- n * (R2full - R2red) / (1 - R2full)
+power.lm.fn <- function(n, alpha, df.test, df.adj, r2.full, r2.red) {
+  r2.part <- (r2.full - r2.red) / (1 - r2.red)  # does this need to be in here?
+  lambda <- n * (r2.full - r2.red) / (1 - r2.full)
   power <-
     stats::pf(
-      q = stats::qf(1 - alpha, df1 = dfTest, df2 = n - dfTest - dfAdj),
-      df1 = dfTest,
-      df2 = n - dfTest - dfAdj,
+      q = stats::qf(1 - alpha, df1 = df.test, df2 = n - df.test - df.adj),
+      df1 = df.test,
+      df2 = n - df.test - df.adj,
       ncp = lambda,
       lower.tail = FALSE
     )
   return(power)
 }
 
+
+#' Get Rsquare from Cohen's d
+#'
+#' Slightly longer description here
+#'
+#' @param d not sure
+#' @param p not sure
+#'
+#' Detailed explanation of what the function does and how
+#'
+d2rsq <- function(d, p) {
+  A <- 1 / (p * (1 - p))
+  rsq <- d ^ 2 / (A + d ^ 2)
+  return(rsq)
+}
